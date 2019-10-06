7c98c07ecb959f8b24896554a7085348148d6372
11Dmitriy11 <dmitriy.usolcev.97@mail.ru>
2019-10-07 01:40:56 +0300

    4

--- script.py
+++ script.py
@@ -28,11 +28,13 @@ def main():
       w.write(seq.read())
   cmd = 'bwa index ./fasta/influensa_hemmaglutinin.fasta'
   os.system(cmd)
+  names = ['id']
+  st = []
+  st1 = []
   with open('result_fastq.txt', 'r') as r:
      links=r.readlines()
   for i in range(len(links)-2):
        name = re.search('\w{3}\d{7}', links[i]).group() 
-       print(name)
        #cmd = f'wget -P fastq {links[i]}'
        #os.system(cmd)
        #cmd = f'gunzip ./fastq/{name}.fastq.gz'
@@ -42,6 +44,35 @@ def main():
        goo = f'cat {name}_varscan_results.vcf'
        cmd = f"{goo} | awk '{go}' >> results.txt"
        os.system(cmd)
+       cmd = f"{goo} | awk '{go}' > res.txt"
+       os.system(cmd)
+       strings=sum(1 for line in open('res.txt'))
+       names += [name for i in range(strings-1)]
+  with open ('results.txt' , 'r') as f:
+       lines =f.readlines()
 
+
+  str_ ='Chrom Position Ref Var Cons:Cov:Reads1:Reads2:Freq:P-value'
+  pattern = re.compile(re.escape(str_))
+  with open('results.txt','w') as f:
+     for i in range(len(lines)):
+        result = pattern.search(lines[i])
+        if (result is None) or (i == 0):
+            f.write(lines[i])
+       
+  with open ('results.txt') as f, open ('results_id.txt', 'w') as f1:
+        for x, y in enumerate(f):
+            y=y.rstrip()
+            id_ =names[x % len(names)]
+            y += ' ' + id_
+            f1.write(y + '\n')
+  with open ('results_id.txt') as f:
+        A=f.read()      
+        A=A.replace(':',' ')   
+  with open('results_id.txt','w') as f:
+         f.write(A)
+  print(A)
+  os.remove('./results.txt')
+  
 if __name__=='__main__':
    main()
